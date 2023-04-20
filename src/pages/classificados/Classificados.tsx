e6bb17ebd56f226ca327296def308b9f9e33e832
Adriel Pereira <adriel.pemi@gmail>
2023-04-20 12:52:21 -0400

    Feat:Melhoria cards

--- src/pages/classificados/Classificados.tsx
+++ src/pages/classificados/Classificados.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, CardBody, CardText, CardTitle, CardGroup, CardSubtitle, CardHeader, Col, Row, Button } from 'reactstrap';
+import { Card, CardBody, CardText, CardTitle, CardGroup, CardSubtitle, CardFooter, CardHeader, Col, Row, Button } from 'reactstrap';
 import { NovoClassificados } from '../novoclassificados/NovoClassificados';
 import { ICriarClassificados } from '../../interfaces';
-import { Container } from './styles';
+
 import { Api } from '../../providers';
 
 
@@ -22,68 +22,51 @@ export const Classificado = () => {
     }
   };
 
+  const excluir = async (Id: number) => {
+    try {
+      await Api.delete(`/api/Classificados/${Id}`);
+      getClassificados();
+    } catch (error) {
+      console.log("err: ", error)
+    }
+  };
+
   useEffect(() => {
     getClassificados()
   }, [])
   return (
-    <Row>
-      <CardGroup>
-        <Col className='col-12  m-0 p-2 pb-4'>
-          <NovoClassificados getClassificados={getClassificados} />
+    <div>
+      <Row className='.container'>
+        <Col xs={12}>
+          <Col className='d-flex justify-content-end ml-auto w-100'>
+            <NovoClassificados getClassificados={getClassificados} />
+          </Col>
+          <CardText>
+            Classificados
+          </CardText>
+          <CardGroup className='d-flex justify-content-center flex-wrap'>
+            {classificados.map((classificado) => (
+              <Col key={classificado.Id} className='m-3 p-2' sm="5" style={{ width: '18rem' }}>
+                <Card body >
+                  <CardHeader className='text-center'>
+                    {classificado.Titulo}
+                  </CardHeader>
+                  <CardText>
+                    {classificado.Descricao}
+                  </CardText>
+                  <CardText>
+                    Data de Cadastro: {new Date(classificado.DataHora).toLocaleDateString('pt-BR')}
+                  </CardText>
+                  <Button color="danger" onClick={() => excluir(classificado.Id)}>Excluir</Button>
+                </Card>
+              </Col>
+            ))}
+          </CardGroup>
         </Col>
-        <Col className='m-3 p-2' sm="5" style={{ width: '18rem' }}>
-          <Card body >
-            <CardHeader>
-              Header
-            </CardHeader>
-            <CardTitle tag="h5">
-              Special Title Treatment
-            </CardTitle>
-            <CardText>
-              With supporting text below as a natural dfasdfasdfasdfasdfasdfasdfasdfasdfasdfaslead-in to additional content.
-            </CardText>
-          </Card>
-        </Col>
-        <Col className='m-3 p-2' sm="5" style={{ width: '18rem' }}>
-          <Card body >
-            <CardHeader>
-              Header
-            </CardHeader>
-            <CardTitle tag="h5">
-              Special Title Treatment
-            </CardTitle>
-            <CardText>
-              With supporting text below as a natural dfasdfasdfasdfasdfasdfasdfasdfasdfasdfaslead-in to additional content.
-            </CardText>
-          </Card>
-        </Col>
-        <Col className='m-3 p-2' sm="5" style={{ width: '18rem' }}>
-          <Card body >
-            <CardHeader>
-              Header
-            </CardHeader>
-            <CardTitle tag="h5">
-              Special Title Treatment
-            </CardTitle>
-            <CardText>
-              With supporting text below as a natural dfasdfasdfasdfasdfasdfasdfasdfasdfasdfaslead-in to additional content.
-            </CardText>
-          </Card>
-        </Col>
-        <Col className='m-3 p-2' sm="5" style={{ width: '18rem' }}>
-          <Card body >
-            <CardHeader>
-              Header
-            </CardHeader>
-            <CardTitle tag="h5">
-              Special Title Treatment
-            </CardTitle>
-            <CardText>
-              With supporting text below as a natural dfasdfasdfasdfasdfasdfasdfasdfasdfasdfaslead-in to additional content.
-            </CardText>
-          </Card>
-        </Col>
-      </CardGroup>
-    </Row>
+      </Row>
+      <CardFooter>
+        Footer
+      </CardFooter>
+    </div>
   );
 };
--- src/pages/novoclassificados/NovoClassificados.tsx
+++ src/pages/novoclassificados/NovoClassificados.tsx
@@ -1,22 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Input, Modal, ModalHeader, ModalBody, ModalFooter, Form } from 'reactstrap';
 import { ICriarClassificados } from '../../interfaces';
-import axios from 'axios';
 import { Api } from '../../providers';
-import { Classificado } from '../classificados/Classificados';
-
 
 interface INovoClassificado {
   getClassificados: () => void
 }
+
+
 
 export const NovoClassificados = ({getClassificados}: INovoClassificado) => {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
 
   const toggleModal = () => setModal(prevState => !prevState);
-  const [classifica, setClassifica] = useState<ICriarClassificados>({ Id: 0, Titulo: '', Descricao: '' , DataHora:''});
+  const [classifica, setClassifica] = useState<ICriarClassificados>({ Id: 0, Titulo: '', Descricao: '', DataHora: '' });
 
 
 
@@ -24,24 +23,23 @@ export const NovoClassificados = ({getClassificados}: INovoClassificado) => {
     const { name, value } = event.target;
     setClassifica(prevUser => ({ ...prevUser, [name]: value }));
   }
- 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const response = await Api.post('/api/Classificados', classifica);
-      response.status === 201 && getClassificados()
+      const response = await Api.post('/api/Classificados', classifica); 
+      response.status === 201 && getClassificados()      
     } catch (error) {
       console.log("err: ", error)
     }
-    setClassifica({ Id: 0, Titulo: '', Descricao: '',DataHora:'' });
+    setClassifica({ Id: 0, Titulo: '', Descricao: '', DataHora: '' });
     toggleModal();
 
   }
 
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '0px' }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '0px', padding: '1vw 25px 0px'}}>
         <Button color="success" onClick={toggleModal} className=' m-0 '>
           + Novo Classificado
         </Button>
@@ -50,7 +48,7 @@ export const NovoClassificados = ({getClassificados}: INovoClassificado) => {
             <ModalHeader toggle={toggleModal}>Novo Classificado</ModalHeader>
             <ModalBody>
               <Input name='Titulo' className='w-100 my-2' onChange={handleChange} value={classifica.Titulo} placeholder='Titulo' />
-              <Input name='Descricao' className='w-100 my-2' onChange={handleChange} value={classifica.Descricao} placeholder='Descrição' />
+              <Input name='Descricao' className='w-100 my-2' onChange={handleChange} value={classifica.Descricao} placeholder='Descrição' />              
             </ModalBody>
             <ModalFooter>
               <Button color="primary" type='submit' >
